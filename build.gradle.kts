3d9daac37e880a25310c13c030ac7bed0f69e916
gugapadilha <guga.santospadilha@rede.ulbra.br>
2026-03-10 20:19:15 -0300

    build gradle test

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,4 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
 }
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
